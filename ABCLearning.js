69e8d7f8281910f6e1f1179d64294e4091922861
devil0118 <925267892@qq.com>
2026-01-07 12:18:44 +0800

    Update ABCLearning.js

--- ABCLearning.js
+++ ABCLearning.js
@@ -3,10 +3,4 @@ ABCLearning - user/info
 */
 
 var obj = JSON.parse($response.body);
-
-if (obj.data) {
-    obj.data.level = "激活码";
-    obj.data.validity_date = Math.floor(Date.now() / 1000) + 315360000;
-}
-console.log(obj)
 $done({ body: JSON.stringify(obj) });
