fe4ce2b382e92fe719f64321dda98182b2d9837f
devil0118 <925267892@qq.com>
2026-01-07 12:23:49 +0800

    Update ABCLearning.js

--- ABCLearning.js
+++ ABCLearning.js
@@ -1,2 +1,43 @@
-$notify("调试", "body前100字符", ($response.body || "空").substring(0, 100));
-$done({});
+/*
+ABCLearning - Notify Debug
+*/
+
+if (typeof $response !== "undefined" && $response.body) {
+    let body = JSON.parse($response.body);
+    let modCount = 0;
+    
+    function modifyObject(obj) {
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                if (typeof obj[key] === "object" && obj[key] !== null) {
+                    modifyObject(obj[key]);
+                } else {
+                    switch (key) {
+                        case "is_vip":
+                            obj[key] = 1;
+                            modCount++;
+                            break;
+                        case "is_lock":
+                            obj[key] = 0;
+                            modCount++;
+                            break;
+                        case "is_free":
+                            obj[key] = 1;
+                            modCount++;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+    
+    modifyObject(body);
+    $response.body = JSON.stringify(body);
+    
+    // 弹出通知显示结果
+    $notify("ABCLearning", "修改了 " + modCount + " 个字段", $request.url);
+} else {
+    $notify("ABCLearning", "⚠️ 响应体为空", $request.url);
+}
+
+$done({ body: $response.body });
