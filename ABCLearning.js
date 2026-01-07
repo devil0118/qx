2ae2e0a42b3674537282f23f2703076b6c247b1a
devil0118 <925267892@qq.com>
2026-01-07 13:14:45 +0800

    Update ABCLearning.js

--- ABCLearning.js
+++ ABCLearning.js
@@ -16,14 +16,11 @@ try {
     } else {
         obj = body;
     }
-    
-    $notify("ABCLearning", "body类型: " + typeof body, "obj类型: " + typeof obj);
-    
     if (obj && obj.data) {
         obj.data.level = "激活码2";
-        obj.data.validity_date = Math.floor(Date.now() / 1000) + 315360000;
+        obj.data.validity_date = 1861199999;
         body = JSON.stringify(obj);
-        $notify("ABCLearning", "✅ 修改成功" + body, "");
+        console.log("ABCLearning ✅ 修改成功" + body);
     }
 } catch (e) {
     $notify("ABCLearning", "❌ 错误", String(e));
