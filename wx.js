6b7bc2932333a48c17fef422d60a6f6f27935c2b
devil0118 <925267892@qq.com>
2026-01-07 17:34:50 +0800

    Update wx.js

--- wx.js
+++ wx.js
@@ -1,50 +1,22 @@
 var body = $response.body;
 var url = $request.url;
-
-// 处理章节信息接口 - 将付费内容的价格改为0
-if (url.indexOf('/book/ChapterInfos') != -1) {
-    body = body.replace(/"price":"[0-9.]+/g, '"price":"0')
-               .replace(/"paid":"d/g, '"paid":"0');
-}
-
-// 处理用户资料接口
-if (url.indexOf('/user/profileArticleId') != -1) {
-    body = body.replace(/location":"[^"]+/g, 'location":"')
-               .replace(/nwesc":"/g, 'nwesc":"https://t.me/iosapp520');
-}
-
-// 处理余额接口 - 修改余额显示
+var body = $response.body;
+var url = $request.url;
+// 处理余额接口 - 将所有余额字段改为 100
 if (url.indexOf('/pay/balance') != -1) {
-    body = body.replace(/"balance":"[0-9.]+/g, '"balance":"999999');
-}
-
-// 处理登录接口 - 模拟VIP会员登录
-if (url.indexOf('/login') != -1) {
-    var obj = {
-        "vid": 943401887,
-        "skey": '0__zhrbh',
-        "accessToken": '0__zhrbh',
-        "refreshToken": '0__zhrbh',
-        "openId": 'onbsirivrc_0ittwod_0eat1',
-        "user": {},
-        "firstLogin": 0,
-        "config": {},
-        "userAgreement": 0
-    };
-    obj.user.name = 'ios鸡神';
-    obj.user.avatar = 'https://telegram-image.pages.dev/file/tcisrieDrtBDsoSes11t.rpg';
-    obj.config.scheme = 'weread://hometab=1';
+    var obj = JSON.parse(body);
+    
+    // 修改所有余额相关字段为 100
+    obj.isPromoting = 100;
+    obj.giftBalance = 100;
+    obj.peerExpiryBalance = 100;
+    obj.peerBalance = 100;
+    obj.paperBalance = 100;
+    obj.expiryBalance = 100;
+    obj.incentive = 100;
+    obj.balance = 100;
+    obj.peerGiftBalance = 100;
+    
     body = JSON.stringify(obj);
 }
-
-// 处理头像上传接口
-if (url.indexOf('upload?type=avatar') != -1) {
-    var obj = {
-        "url": 'https://telegram-image.pages.dev/file/tcisrieDrtBDsoSes11t.rpg'
-    };
-    body = JSON.stringify(obj);
-}
-console.log("weixin========"+body)
-$done({
-    "body": body
-});
+$done({ body });
