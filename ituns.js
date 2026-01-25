d72150aff6ee89bf62ff3554bc2b61ce00c3b173
devil0118 <925267892@qq.com>
2026-01-25 13:37:40 +0800

    Update ituns.js

--- ituns.js
+++ ituns.js
@@ -12,7 +12,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/devil0118/qx/refs/heads/main/ituns.js
 
 [mitm]
 hostname = buy.itunes.apple.com
